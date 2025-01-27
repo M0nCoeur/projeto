1af8d4a2e7360f1d4ed2bd86d2e6d045cbb811a1
M0nCoeur <lucas.contato020@outlook.com>
2025-01-27 08:32:41 -0300

    Banco de dados.

--- db/gameverse.sql
+++ db/gameverse.sql
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 -- Banco de dados: `gameverse`
 --
 
+CREATE DATABASE IF NOT EXISTS `gameverse` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `gameverse`;
 -- --------------------------------------------------------
 
 --
